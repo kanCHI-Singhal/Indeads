301572bb8a6ca4ba18f1292213343112d409cc1e
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-08-25 21:29:22 +0530

    Added heading for InDiBUS by Ganesh
    > added h1

--- indeads/src/app/aboutIndiabus/page.js
+++ indeads/src/app/aboutIndiabus/page.js
@@ -3,7 +3,7 @@ import styles from"../page.module.css"
 export default function aboutIndibus(){
     return(
         <main className={styles.main}>
-
+            <h1 className={styles.Typewriter}> InDiBUS </h1>
         </main>
     )
 }
