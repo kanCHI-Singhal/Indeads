72148611559e493b8aa3e333b603946dca29e96c
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-08-25 20:43:05 +0530

    Routing Changes
    >Routing changes.
    >Content Area created.

---
+++ indeads/src/app/aboutIndiabus/page.js
@@ -0,0 +1,9 @@
+import styles from"../page.module.css"
+
+export default function aboutIndibus(){
+    return(
+        <main className={styles.main}>
+
+        </main>
+    )
+}
--- indeads/src/app/layout.js
+++ indeads/src/app/layout.js
@@ -27,6 +27,9 @@ export default function RootLayout({ children }) {
                     <div className='flexContent' ><Link href='/report' className='pagelink'> Report </Link></div>
                     <div className='flexContent' ><Link href='/serve' className='pagelink'> Our Services </Link></div>
                     <div className='flexContent' ><Link href='/screenc' className='pagelink'> Screen Check </Link></div>
+                    <div className='flexContent' ><Link href='/aboutIndibus' className='pagelink'> About Us </Link></div>
+                    <div className='flexContent' ><Link href='/aboutIndiads' className='pagelink'> InDiAds </Link></div>
+                    <div className='flexContent' ><Link href='/Indilearn' className='pagelink'> InDiLearn </Link></div>
                 </div>
             </div>
             <div className='item mainbody'>
