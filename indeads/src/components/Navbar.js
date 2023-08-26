3c4e7ff79b44022507fc8e690bdea8057598de3e
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-08-26 21:10:51 +0530

    UI changes

--- indeads/src/components/Navbar.js
+++ indeads/src/components/Navbar.js
@@ -3,7 +3,6 @@ import { SocialIcon } from 'react-social-icons'
 import Link from 'next/link'
 import React,{ useState, useEffect} from 'react'
 import "../app/globals.css"
-import Document from 'next/document'
 
 
 
