7c8a6c7f26367622fab25379f862546f2511bb5f
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-08-28 19:57:12 +0530

    Components removed

--- indeads/src/components/Navbar.js
+++ indeads/src/components/Navbar.js
@@ -3,14 +3,13 @@ import { SocialIcon } from 'react-social-icons'
 import Link from 'next/link'
 import React,{ useState, useEffect} from 'react'
 import "../app/globals.css"
-import Collapsible from 'react-collapsible'
 
 
 export default function Navbar (){
 
     return(
       <nav className='nav-parent'>
-      <Collapsible trigger="click me">
+
           <ul className = "nav">
                 <li className='nav-link' >
                   <Link className='link' href="/">Home</Link>
@@ -25,7 +24,7 @@ export default function Navbar (){
                   <Link className='link' href="/">Disabled</Link>
                 </li>
               </ul>
-      </Collapsible>  
+
       </nav>
     )
 }
